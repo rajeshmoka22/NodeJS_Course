da2b127c53350e0beb8ce882a648615956d41287
Rajesh Moka <rajeshmoka@Rajeshs-MacBook.local>
2022-12-31 17:18:09 +0530

    homework2 code changes

---
+++ task2/schema.ts
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+import {Request, Response, NextFunction} from 'express';
+
+export const userSchema = Joi.object({
+  login: Joi.string().required(),
+  password: Joi.string().alphanum().required(),
+  age: Joi.number().integer().min(4).max(130).required()
+});
+
+type UserSchema = typeof userSchema;
+
+export const userValidator = (schema: UserSchema) => (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body); 
+  const valid = error == null;
+  if (valid) { 
+    next(); 
+  } else {
+    res.status(400).send({message: error?.details?.[0]?.message || 'Bad request'});
+  }
+}
